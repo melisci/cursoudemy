d711730077ae88b7e8dff4d2dcaf77478222b5c3
melisci <melisascime@gmail.com>
2024-04-21 16:07:14 -0300

    primera parte del trabajo bienes raices

--- 008bienesraices_inicio/src/js/app.js
+++ 008bienesraices_inicio/src/js/app.js
@@ -6,6 +6,21 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function darkMode(){
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme:dark)');
+
+    if (prefiereDarkMode.matches){
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+    prefiereDarkMode.addEventListener('change', function(){
+        if (prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    })
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
     botonDarkMode.addEventListener('click', function(){
         document.body.classList.toggle('dark-mode');
